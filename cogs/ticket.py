d4258c0e811344c8f65700c99e05a8920441c455
Amxgh <86120332+Amxgh@users.noreply.github.com>
2021-06-29 19:53:07 +0400

    Update ticket.py

--- cogs/ticket.py
+++ cogs/ticket.py
@@ -20,7 +20,7 @@ class Tickets(commands.Cog, name="Tickets"):
                             request = await request.json()
                             await session.close()
 
-                   if resp.status != 200:
+                   if 'error' in request:
                         await ctx.send('Please enter a valid ign!')
                     else:
                         ign = request['name']
