3ef6dd541dd1a423b85d815df9a7bfa54ca5bbfe
Amxgh <86120332+Amxgh@users.noreply.github.com>
2021-07-29 01:39:19 +0400

    Fixed Code
    • Removed stuff from after_cache_ready and moved it with the other variables.
    • Neatened do-not-kick-list tickets.
    • Added an auto-fix for incorrectly entered dates in do-not-kick-list tickets.
    • Added an approval message for do-not-kick-list tickets when they're approved by staff members.
    • Fixed Rolecheck.
    • Replaced local variables in Register with bot variables.

--- cogs/hypixel.py
+++ cogs/hypixel.py
@@ -33,7 +33,7 @@ class Hypixel(commands.Cog, name="Hypixel"):
 
                 await author.edit(nick=ign)
                 if guild_name == "Miscellaneous":
-                    if tag != None and len([role for role in ctx.author.roles if role in (self.bot.active_role, self.bot.staff, self.bot.former_staff, self.bot.server_booster)]):
+                    if tag != None and [role for role in ctx.author.roles if role in (self.bot.active_role, self.bot.staff, self.bot.former_staff, self.bot.server_booster)]:
                         with open('badwords.txt', 'r') as f:
                             badwords = f.read()
                         if tag.isascii() is False:
@@ -42,14 +42,14 @@ class Hypixel(commands.Cog, name="Hypixel"):
                         elif len(tag) > 6:
                             await ctx.send("The tag may not be longer than 6 characters. Re-run the command if you wish to add a tag!")
                         elif tag.lower() in badwords.split('\n'):
-                            await ctx.send("The tag may not include profane language. Re-run the command if you wish to add a tag!")
+                            await ctx.send("The tag may not include profanity. Re-run the command if you wish to add a tag!")
                         else: # Set user's tag
                             new_nick = ign + f' [{tag}]'
                             await author.edit(nick=new_nick)
                     elif tag == None:
                         pass
                     else: # 'tag' argument is NOT None, but user doesn't have roles
-                        await ctx.send("Your tag will not be updated since you do not have the active/staff/server booster role!")
+                        await ctx.send("Your tag will not be updated since you do not have the active/staff/former staff/server booster role!")
                     
 
                     await ctx.author.remove_roles(self.bot.guest, self.bot.awaiting_app)
@@ -107,7 +107,7 @@ class Hypixel(commands.Cog, name="Hypixel"):
                 await ctx.author.edit(nick=ign)
                 guild_name = await hypixel.get_guild(name)
                 if guild_name == "Miscellaneous":
-                    if len([role for role in ctx.author.roles if role in (self.bot.active_role, self.bot.staff, self.bot.former_staff, self.bot.server_booster)]):
+                    if [role for role in ctx.author.roles if role in (self.bot.active_role, self.bot.staff, self.bot.former_staff, self.bot.server_booster)]:
                         while True:
                             embed = discord.Embed(title="What would you like your tag to be? ",
                                                   url="https://media.discordapp.net/attachments/420572640172834816/867506975884181554/unknown.png",
@@ -132,7 +132,7 @@ class Hypixel(commands.Cog, name="Hypixel"):
                             elif len(tag) > 6:
                                 await ctx.send("The tag may not be longer than 6 characters.")
                             elif tag.lower() in badwords.split('\n'):
-                                await ctx.send("The tag may not include profane language")
+                                await ctx.send("The tag may not include profanity.")
                             else:
                                 new_nick = ign + f' [{tag}]'
                                 await author.edit(nick=new_nick)
