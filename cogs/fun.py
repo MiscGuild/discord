5889fc52ec98d66106b4abebfb2930f433be286c
Amxgh <86120332+Amxgh@users.noreply.github.com>
2021-08-05 18:13:56 +0400

    Removed chester

--- cogs/fun.py
+++ cogs/fun.py
@@ -24,19 +24,6 @@ class Fun(commands.Cog, name="Fun"):
         embed.set_image(url=req['image'])
         await ctx.send(embed=embed)
 
-    @commands.command()
-    async def chester(self, ctx):
-        """Sends a random image of chester
-        """
-        links = ['https://bit.ly/3zVao27', 'https://bit.ly/3wTpNOK', 'https://bit.ly/3wTpNOK', 'https://bit.ly/2T2gXiX',
-                 'https://bit.ly/3qjLS6F', 'https://bit.ly/3h1zJyH', 'https://bit.ly/2SYlQcN', 'https://bit.ly/3xN3vy5',
-                 'https://bit.ly/3j3sZDj', 'https://bit.ly/35Lu9v8', 'https://bit.ly/35Lu9v8', 'https://bit.ly/2Usk2Jn',
-                 'https://bit.ly/3wSxTXS', 'https://bit.ly/3qnEYNO', 'https://bit.ly/3gOwJa3', 'https://bit.ly/3zVSVXE',
-                 'https://bit.ly/3zKYDv8', 'https://bit.ly/3xNHn6U', 'https://bit.ly/35MdUOz', 'https://bit.ly/35MHfIJ']
-        image = links[random.randint(0, len(links) + 1)]
-        embed = discord.Embed(title="Here's the furry you requested:", color=0xD2691e)
-        embed.set_image(url=image)
-        await ctx.send(embed=embed)
 
     # Ping
     @commands.command()
