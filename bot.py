dc34ed5e9daf8c6274acab69f1d7e6d18393f9ed
Amxgh <86120332+Amxgh@users.noreply.github.com>
2021-08-05 18:47:43 +0400

    fixed error

--- bot.py
+++ bot.py
@@ -1249,8 +1249,8 @@ async def ticketer():
                                                                   category=category,
                                                                   topic="<:t:869239368060112906><:i:869239367942697010><:c:869239368383074414>"
                                                                         "<:k:869239367854612480><:e:869239368517287936><:t:869239368060112906>")
-        creating_ticket = discord.Embed(title="Click here to go to your ticket!", url=f"https://discord.com/channels/{bot.misc_guild}/{ticket_channel.id}")
-        creating_ticket.set_author(name="Ticket successfully created!", color=0x00A86B)
+        creating_ticket = discord.Embed(title="Click here to go to your ticket!", url=f"https://discord.com/channels/{bot.misc_guild}/{ticket_channel.id}", color=0x00A86B)
+        creating_ticket.set_author(name="Ticket successfully created!")
 
         await click.respond(type=InteractionType.ChannelMessageWithSource, embed=creating_ticket)
 
