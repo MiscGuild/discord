2423af3926717a26db37a7046d5736ff014530df
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2021-08-26 14:52:41 +1000

    re-do DNKL table creation

--- bot.py
+++ bot.py
@@ -142,6 +142,10 @@ async def on_member_join(member):
 
 async def connect_db():
     bot.db = await aiosqlite.connect("database.db")
+    await bot.db.execute("""CREATE TABLE IF NOT EXISTS DNKL (
+	message_id integer NOT NULL,
+	username text NOT NULL)""")
+    await bot.db.commit()
     print("db connected")
 
 
