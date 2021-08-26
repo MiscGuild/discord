2625a2c6277318b119506235c306934c064cb912
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2021-08-26 15:32:23 +1000

    Fix Giveaways
    Indentation bad.

--- bot.py
+++ bot.py
@@ -146,6 +146,19 @@ async def connect_db():
 	message_id integer NOT NULL,
 	username text NOT NULL)""")
     await bot.db.commit()
+
+    await bot.db.execute('''CREATE TABLE IF NOT EXISTS Giveaways (
+    message_id integer PRIMARY KEY NOT NULL,
+    channel_id integer NOT NULL,
+    prize text NOT NULL,
+    number_winners integer NOT NULL,
+    time_of_finish text NOT NULL,
+    role_requirement_type text NOT NULL,
+    required_roles text,
+    required_gexp integer NOT NULL,
+    sponsors text NOT NULL,
+    status text NOT NULL)''')
+    await bot.db.commit()
     print("db connected")
 
 
