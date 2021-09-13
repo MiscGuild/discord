6adc8f6033576331d44c167f80eb6f3ebd609649
Amxgh <86120332+Amxgh@users.noreply.github.com>
2021-09-13 21:09:28 +0400

    New command ,beer

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,7 +2,7 @@ import random
 
 import aiohttp
 import discord
-import requests
+import asyncio
 import secrets
 from discord.ext import commands
 
@@ -158,6 +158,41 @@ class Fun(commands.Cog, name="Fun"):
         if hasattr(ctx, "guild") and ctx.guild is not None:
             await ctx.send(f"Sending you a private message with your random generated password **{ctx.author.name}**")
         await ctx.author.send(f"🎁 **Here is your password:**\n{secrets.token_urlsafe(nbytes)}")
+
+    @commands.command()
+    async def beer(self, ctx, user: discord.Member = None, *, reason: commands.clean_content = ""):
+        """ Give someone a beer! 🍻 """
+        if not user or user.id == ctx.author.id:
+            return await ctx.send(f"**{ctx.author.name}**: paaaarty!🎉🍺")
+        if user.id == self.bot.user.id:
+            return await ctx.send("*drinks beer with you* 🍻")
+        if user.bot:
+            return await ctx.send(
+                f"I would love to give beer to the bot **{ctx.author.name}**, but I don't think it will respond to you :/")
+
+        beer_offer = f"**{user.name}**, you got a 🍺 offer from **{ctx.author.name}**"
+        beer_offer = beer_offer + f"\n\n**Reason:** {reason}" if reason else beer_offer
+        msg = await ctx.send(beer_offer)
+
+        def reaction_check(m):
+            if m.message_id == msg.id and m.user_id == user.id and str(m.emoji) == "🍻":
+                return True
+            return False
+
+        try:
+            await msg.add_reaction("🍻")
+            await self.bot.wait_for("raw_reaction_add", timeout=30.0, check=reaction_check)
+            await msg.edit(content=f"**{user.name}** and **{ctx.author.name}** are enjoying a lovely beer together 🍻")
+        except asyncio.TimeoutError:
+            await msg.delete()
+            await ctx.send(f"well, doesn't seem like **{user.name}** wanted a beer with you **{ctx.author.name}** ;-;")
+        except discord.Forbidden:
+            # Yeah so, bot doesn't have reaction permission, drop the "offer" word
+            beer_offer = f"**{user.name}**, you got a 🍺 from **{ctx.author.name}**"
+            beer_offer = beer_offer + f"\n\n**Reason:** {reason}" if reason else beer_offer
+            await msg.edit(content=beer_offer)
+
+
 
     @commands.command()
     async def joke(self, ctx):
