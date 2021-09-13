7023f5b37a6878158a0f5f4560f2f658e0208e2f
Amxgh <86120332+Amxgh@users.noreply.github.com>
2021-09-13 21:15:54 +0400

    New command ,source

--- cogs/general.py
+++ cogs/general.py
@@ -7,8 +7,12 @@ class General(commands.Cog, name="General"):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def source(self, ctx):
+        """ Check the source code!"""
+        await ctx.send(
+            f"**{ctx.bot.user}** is powered by this source code:\nhttps://github.com/MiscGuild/MiscBot")
 
-
-
+    
 def setup(bot):
     bot.add_cog(General(bot))
