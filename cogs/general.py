8aa19968c0ff283a27ec0a0e92cbe23e79cd8e81
Amxgh <86120332+Amxgh@users.noreply.github.com>
2021-09-14 08:15:39 +0400

    Changed ,source

--- cogs/general.py
+++ cogs/general.py
@@ -1,6 +1,8 @@
-import chat_exporter
+
 import discord
 import aiohttp
+import inspect
+import os
 from discord.ext import commands
 
 
@@ -9,10 +11,43 @@ class General(commands.Cog, name="General"):
         self.bot = bot
 
     @commands.command()
-    async def source(self, ctx):
-        """ Check the source code!"""
-        await ctx.send(
-            f"**{ctx.bot.user}** is powered by this source code:\nhttps://github.com/MiscGuild/MiscBot")
+    async def source(self, ctx, *, command: str = None):
+        """Displays my full source code or for a specific command.
+        To display the source code of a subcommand you can separate it by
+        periods, e.g. tag.create for the create subcommand of the tag command
+        or by spaces.
+        """
+        source_url = 'https://github.com/MiscGuild/MiscBot'
+        branch = 'main'
+        if command is None:
+            return await ctx.send(source_url)
+
+        if command == 'help':
+            src = type(self.bot.help_command)
+            module = src.__module__
+            filename = inspect.getsourcefile(src)
+        else:
+            obj = self.bot.get_command(command.replace(',', ' '))
+            if obj is None:
+                return await ctx.send('Could not find command.')
+
+            # since we found the command we're looking for, presumably anyway, let's
+            # try to access the code itself
+            src = obj.callback.__code__
+            module = obj.callback.__module__
+            filename = src.co_filename
+
+        lines, firstlineno = inspect.getsourcelines(src)
+        if not module.startswith('discord'):
+            # not a built-in command
+            location = os.path.relpath(filename).replace('\\', '/')
+        else:
+            location = module.replace('.', '/') + '.py'
+            source_url = 'https://github.com/MiscGuild/MiscBot'
+            branch = 'main'
+
+        final_url = f'<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>'
+        await ctx.send(f"Following is the source code for {command}\n{final_url}")
 
     @commands.command()
     async def ping(self, ctx):
@@ -57,5 +92,6 @@ class General(commands.Cog, name="General"):
                 f"**{country.capitalize()}** *({r['countryInfo']['iso3']})*",
                 embed=embed
             )
+    
 def setup(bot):
     bot.add_cog(General(bot))
