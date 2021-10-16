3accd0be96e967c3bc1490828ac6c25c66e49acb
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2021-10-16 12:06:31 +1100

    Outline Member.py

---
+++ func/classes/Member.py
@@ -0,0 +1,27 @@
+# The following file contains: mute, unmute, kick, ban, softban, unban, forcesync, add, remove, accept
+
+import discord
+
+class Member:
+    def __init__(self, member: discord.Member):
+        self.member = member
+
+    # async def mute(reason=None):
+
+    # async def unmute():
+
+    # async def kick(reason=None):
+
+    # async def ban(reason=None):
+
+    # async def softban(reason=None):
+
+    # async def unban():
+
+    # async def forcesync(new_name):
+
+    # async def add():
+
+    # async def remove():
+
+    # async def accept():
