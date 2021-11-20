0df928918bfc3e7340794a158194a0e9405608d2
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2021-11-20 11:57:15 +1100

    Create consts.py

---
+++ func/utils/consts.py
@@ -0,0 +1,10 @@
+import discord
+
+neg_color = 0xDE3163
+pos_color = 0x00A86B
+neutral_color = 0x8368ff
+
+unknown_ign_embed = discord.Embed(title="Please enter a valid Minecraft username!", color=neg_color)
+unknown_ign_msg = "Unkown IGN!"
+
+staff_impersonation_embed = discord.Embed(title="Staff impersonation is a punishable offense!", color=neg_color)
