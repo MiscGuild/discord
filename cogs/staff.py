171eaf20d98b3bce9924a4ff573fc3837c98659c
Amxgh <86120332+Amxgh@users.noreply.github.com>
2021-12-30 19:39:09 +0400

    Fixed ,inactive & Removed ,challenge

--- cogs/staff.py
+++ cogs/staff.py
@@ -99,7 +99,7 @@ class staff(commands.Cog, name="Staff"):
 
         dnkl_names = []
         for tuple in rows:
-            username = tuple
+            username = tuple[0]
             dnkl_names.append(username)
 
         activearray = {}
@@ -273,8 +273,6 @@ class staff(commands.Cog, name="Staff"):
 
             await msg.delete()
 
-
-
     @commands.command(aliases=['fs'])
     @commands.has_role("Staff")
     async def forcesync(self, ctx, member: discord.Member, name):
@@ -418,94 +416,6 @@ class staff(commands.Cog, name="Staff"):
                               color=0x8368ff)
         partner_channel = self.bot.get_channel(874725728397193436)
         await partner_channel.send(embed=embed)
-
-    @commands.command()
-    @commands.has_role("Staff")
-    async def challenge(self, ctx, x):
-        channel = self.bot.get_channel(753103243659444286)
-        if x == "e":
-            msg = await ctx.send(
-                content="**What would you like the first challenge under the easy category to be (name)?**")
-            challenge1 = await self.bot.wait_for('message', check=lambda x: x.author == ctx.message.author)
-            challenge1 = challenge1.content
-
-            await msg.edit(content="**What is the prize for completing this challenge?**")
-            challenge1_prize = await self.bot.wait_for('message', check=lambda x: x.author == ctx.message.author)
-            challenge1_prize = challenge1_prize.content
-
-            await msg.edit(
-                content="**What would you like the second challenge under the easy category to be?"
-                        "\nIf you don't want one, type None**")
-            challenge2 = await self.bot.wait_for('message', check=lambda x: x.author == ctx.message.author)
-            challenge2 = challenge2.content
-            if challenge2 == "None":
-                embed = discord.Embed(title="Easy", color=0x90ee90)
-                embed.add_field(name=challenge1, value=challenge1_prize, inline=False)
-            else:
-                await msg.edit(content="What is the prize for completing this challenge?")
-                challenge2_prize = await self.bot.wait_for('message', check=lambda x: x.author == ctx.message.author)
-                challenge2_prize = challenge2_prize.content
-
-                embed = discord.Embed(title="Easy", color=0x90ee90)
-                embed.add_field(name=challenge1, value=challenge1_prize, inline=False)
-                embed.add_field(name=challenge2, value=challenge2_prize, inline=False)
-            await channel.send(f'*Complete the following challenges to get prizes*\nTo view the store, use `!shop`')
-            await channel.send(embed=embed)
-
-        if x == "m":
-            msg = await ctx.send("**What would you like the first challenge under the medium category to be (name)?**")
-            challenge1 = await self.bot.wait_for('message', check=lambda x: x.author == ctx.message.author)
-            challenge1 = challenge1.content
-
-            await msg.edit(content="**What is the prize for completing this challenge?**")
-            challenge1_prize = await self.bot.wait_for('message', check=lambda x: x.author == ctx.message.author)
-            challenge1_prize = challenge1_prize.content
-
-            await msg.edit(
-                content="**What would you like the second challenge under the medium category to be?"
-                        "\nIf you don't want one, type None**")
-            challenge2 = await self.bot.wait_for('message', check=lambda x: x.author == ctx.message.author)
-            challenge2 = challenge2.content
-            if challenge2 == "None":
-                embed = discord.Embed(title="Medium", color=0xffa500)
-                embed.add_field(name=challenge1, value=challenge1_prize, inline=False)
-            else:
-                await msg.edit(content="**What is the prize for completing this challenge?**")
-                challenge2_prize = await self.bot.wait_for('message', check=lambda x: x.author == ctx.message.author)
-                challenge2_prize = challenge2_prize.content
-
-                embed = discord.Embed(title="Medium", color=0xffa500)
-                embed.add_field(name=challenge1, value=challenge1_prize, inline=False)
-                embed.add_field(name=challenge2, value=challenge2_prize, inline=False)
-            await channel.send(embed=embed)
-
-        if x == "h":
-            msg = await ctx.send("**What would you like the challenge under the hard category to be (name)?**")
-            challenge1 = await self.bot.wait_for('message', check=lambda x: x.author == ctx.message.author)
-            challenge1 = challenge1.content
-
-            await msg.edit(content="**What is the prize for completing this challenge?**")
-            challenge1_prize = await self.bot.wait_for('message', check=lambda x: x.author == ctx.message.author)
-            challenge1_prize = challenge1_prize.content
-
-            await msg.edit(
-                content="**What would you like the second challenge under the hard category to be?"
-                        "\nIf you don't want one, type None**")
-            challenge2 = await self.bot.wait_for('message', check=lambda x: x.author == ctx.message.author)
-            challenge2 = challenge2.content
-            if challenge2 == "None":
-                embed = discord.Embed(title="Hard", color=0xcd5c5c)
-                embed.add_field(name=challenge1, value=challenge1_prize, inline=False)
-            else:
-                await msg.edit(content="**What is the prize for completing this challenge?**")
-                challenge2_prize = await self.bot.wait_for('message', check=lambda x: x.author == ctx.message.author)
-                challenge2_prize = challenge2_prize.content
-
-                embed = discord.Embed(title="Hard", color=0xcd5c5c)
-                embed.add_field(name=challenge1, value=challenge1_prize, inline=False)
-                embed.add_field(name=challenge2, value=challenge2_prize, inline=False)
-                embed.set_footer(text="You can only do one challenge once.")
-            await channel.send(embed=embed)
 
     @commands.command()
     @commands.has_role("Staff")
