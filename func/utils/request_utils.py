006c76bf2f0bc4f6b3a1a871c926e649dc052ad5
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-01-13 23:03:28 +1100

    perf(rewrite): add get_json_response util to minify code and catch errors

--- func/utils/request_utils.py
+++ func/utils/request_utils.py
@@ -1,6 +1,7 @@
 import aiohttp
 import toml
 import random
+import re
 import discord
 from io import BytesIO
 
@@ -11,13 +12,24 @@ api_keys = toml.load("config.toml")["hypixel"]["api_keys"]
 async def get_hyapi_key():
     return random.choice(api_keys)
 
+# Base JSON-getter for all JSON based requests. Catches Invalid API Key errors
+async def get_json_response(url: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            resp = await resp.json(content_type=None)
+            await session.close()
+
+    # Check for invalid API keys
+    if "cause" in resp and resp["cause"] == "Invalid API key":
+        print(f"WARNING: The API key {re.search(r'(?<=key=)(.*?)(?=&)', url).group(0)} is invalid!")
+
+    # Return JSON response
+    return resp
+
 
 # Returns the Mojang profile of a player
 async def get_mojang_profile(name: str):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://api.mojang.com/users/profiles/minecraft/{name}") as resp:
-            resp = await resp.json(content_type=None)
-            await session.close()
+    resp = await get_json_response(f"https://api.mojang.com/users/profiles/minecraft/{name}")
 
     # If player and request is valid
     if resp and "error" not in resp:
@@ -29,10 +41,7 @@ async def get_mojang_profile(name: str):
 
 # Returns the Mojang profile of a player (including skin)
 async def get_name_by_uuid(uuid: str):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://api.mojang.com/user/profiles/{uuid}/names") as resp:
-            resp = await resp.json(content_type=None)
-            await session.close()
+    resp = await get_json_response(f"https://api.mojang.com/user/profiles/{uuid}/names")
 
     # If player and request is valid
     if resp and "error" not in resp:
@@ -45,11 +54,7 @@ async def get_name_by_uuid(uuid: str):
 # Returns a player's guild if they're in one
 async def get_player_guild(uuid):
     api_key = await get_hyapi_key()
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://api.hypixel.net/guild?key={api_key}&player={uuid}") as resp:
-            resp = await resp.json()
-            await session.close()
+    resp = await get_json_response(f"https://api.hypixel.net/guild?key={api_key}&player={uuid}")
 
     # Player is not in a guild
     if "guild" not in resp or not resp["guild"]:
@@ -62,11 +67,7 @@ async def get_player_guild(uuid):
 # Returns guild data by name
 async def get_guild_by_name(name):
     api_key = await get_hyapi_key()
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://api.hypixel.net/guild?key={api_key}&name={name}") as resp:
-            resp = await resp.json()
-            await session.close()
+    resp = await get_json_response(f"https://api.hypixel.net/guild?key={api_key}&name={name}")
 
     # Player is not in a guild
     if "guild" not in resp or not resp["guild"]:
@@ -79,11 +80,7 @@ async def get_guild_by_name(name):
 # Returns the tag of a given guild
 async def get_gtag(name):
     api_key = await get_hyapi_key()
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://api.hypixel.net/guild?key={api_key}&name={name}") as resp:
-            resp = await resp.json(content_type=None)
-            await session.close()
+    resp = await get_json_response(f"https://api.hypixel.net/guild?key={api_key}&name={name}")
 
     if len(resp["guild"]) < 2:
         return (" ")
@@ -97,11 +94,7 @@ async def get_gtag(name):
 # Returns Hypixel player data
 async def get_hypixel_player(name: str):
     api_key = await get_hyapi_key()
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://api.hypixel.net/player?key={api_key}&name={name}") as resp:
-            resp = await resp.json(content_type=None)
-            await session.close()
+    resp = await get_json_response(f"https://api.hypixel.net/player?key={api_key}&name={name}")
 
     # Player doesn't exist
     if "player" not in resp or not resp["player"]:
