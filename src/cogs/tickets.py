47a6658f74446c494a995d820a84831e3b40f3ad
Amxgh <86120332+Amxgh@users.noreply.github.com>
2022-07-26 17:07:14 +0400

    feat: moved cogs.tickets from commands to bridge

--- src/cogs/tickets.py
+++ src/cogs/tickets.py
@@ -1,5 +1,6 @@
 import discord
-from discord.ext import commands
+
+from discord.ext import commands, bridge
 from src.func.General import General
 from src.func.Listener import Listener
 from src.func.String import String
@@ -14,100 +15,105 @@ class Tickets(commands.Cog, name="tickets"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=["reg", "verify"])
+    @bridge.bridge_command(aliases=["reg", "verify"])
     async def register(self, ctx, name: str):
         """Register with your IGN to sync your roles!"""
         res = await Union(user=ctx.author).register(ctx, name)
         if isinstance(res, discord.Embed):
-            await ctx.send(embed=res)
+            await ctx.respond(embed=res)
         if isinstance(res, String):
-            await ctx.send(res)
+            await ctx.respond(res)
 
-    @commands.command(aliases=["del"])
+    @bridge.bridge_command(aliases=["del"])
     @commands.has_role("Staff")
     async def delete(self, ctx):
         """Delete a ticket!"""
         res = await General.delete(ctx)
         if res:
-            await ctx.send(res)
+            await ctx.respond(res)
 
-    @commands.command()
+    @bridge.bridge_command()
     @commands.has_role("Staff")
     async def add(self, ctx, member: discord.Member):
         """Add a user to a ticket!"""
         res = await Union(user=member).add(ctx)
         if isinstance(res, str):
-            await ctx.send(res)
+            await ctx.respond(res)
         elif isinstance(res, discord.Embed):
-            await ctx.send(embed=res)
+            await ctx.respond(embed=res)
 
-    @commands.command()
+    @bridge.bridge_command()
     @commands.has_role("Staff")
     async def remove(self, ctx, member: discord.Member):
         """Remove a user from a ticket!"""
         res = await Union(user=member).remove(ctx)
         if isinstance(res, str):
-            await ctx.send(res)
+            await ctx.respond(res)
         elif isinstance(res, discord.Embed):
-            await ctx.send(embed=res)
+            await ctx.respond(embed=res)
 
-    @commands.command()
+    @bridge.bridge_command()
     @commands.has_role("Staff")
     async def rename(self, ctx, *, channel_name: str):
         """Rename a ticket!"""
-        await String(string=channel_name).rename(ctx)
+        await ctx.respond(embed=await String(string=channel_name).rename(ctx))
 
-    @commands.command()
+    @bridge.bridge_command()
     @commands.has_role("Staff")
     async def transcript(self, ctx):
         """Create a transcript for a ticket!"""
         res = await General.transcript(ctx)
         if isinstance(res, discord.Embed):
-            await ctx.send(embed=res)
+            await ctx.respond(embed=res)
         elif isinstance(res, discord.File):
-            await ctx.send(file=res)
+            await ctx.respond(file=res)
         elif isinstance(res, str):
-            await ctx.send(res)
+            await ctx.respond(res)
 
-    @commands.command()
+    @bridge.bridge_command()
     @commands.has_role("Admin")
     async def accept(self, ctx):
         """Accept a staff application!"""
         res = await General.accept(ctx)
         if isinstance(res, str):
-            await ctx.send(res)
+            await ctx.respond(res)
         if isinstance(res, discord.Embed):
-            await ctx.send(embed=res)
+            await ctx.respond(embed=res)
 
-    @commands.command()
+    @bridge.bridge_command()
     @commands.has_any_role("Admin", "Moderator")
     async def deny(self, ctx, channel: discord.TextChannel):
         """Deny a staff application!"""
         # Get result and send file if it is returned
         embed, file = await General.deny(ctx, channel)
-        await channel.send(embed=embed)
+        await channel.respond(embed=embed)
         if file:
-            return await channel.send(file=file)
+            return await channel.respond(file=file)
 
-    @commands.command()
+    @bridge.bridge_command()
     async def new(self, ctx):
         """Create a new ticket!"""
-        await ctx.send(await General.new(ctx))
+        await ctx.respond(await General.new(ctx))
 
-    @commands.command(aliases=['AddMilestone'])
-    async def MilestoneAdd(self, ctx):
+    @bridge.bridge_command(aliases=['AddMilestone'])
+    @commands.has_role('Staff')
+    async def milestoneadd(self, ctx):
         """Register a milestone"""
-        await General.add_milestone(ctx)
+        embed, view = await General.add_milestone(ctx)
+        await ctx.respond(embed=embed, view=view)
 
-    @commands.command(aliases=['UpdateMilestone'])
-    async def MilestoneUpdate(self, ctx):
+    @bridge.bridge_command(aliases=['UpdateMilestone'])
+    @commands.has_role('Staff')
+    async def milestoneupdate(self, ctx):
         """Update a milestone that has already been registered"""
-        await General.update_milestone(ctx)
+        embed, view = await General.update_milestone(ctx)
+        await ctx.respond(embed=embed, view=view)
 
-    @commands.command(aliases=["CompileMilestones", "mc", "cm", "MilestoneCompile", "CompileMilestone"])
-    async def MilestonesCompile(self, ctx):
+    @bridge.bridge_command(aliases=["CompileMilestones", "mc", "cm", "CompileMilestone"])
+    @commands.has_role('Staff')
+    async def milestonecompile(self, ctx):
         """Compiles all milestones into one message and sends it to the milestones channel"""
-        await General.compile_milestones(ctx)
+        await ctx.respond(await General.compile_milestones(ctx))
 
     @commands.Cog.listener()
     async def on_interaction(self, res):
