74798a9773fd93c077ae9799f027ea4229ebe060
Amxgh <86120332+Amxgh@users.noreply.github.com>
2022-07-26 17:04:00 +0400

    feat: moved cogs.hypixel from commands to bridge

--- src/cogs/hypixel.py
+++ src/cogs/hypixel.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, bridge
 from src.func.General import General
 from src.func.String import String
 from src.func.Union import Union
@@ -14,54 +14,54 @@ class Hypixel(commands.Cog, name="hypixel"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def sync(self, ctx, name: str, tag: str = None):
+    @bridge.bridge_command()
+    async def sync(self, ctx, name, tag: str = None):
         """Update your discord nick, tag and roles!"""
         res = await Union(user=ctx.author).sync(ctx, name, tag)
         if isinstance(res, discord.Embed):
-            await ctx.send(embed=res)
+            await ctx.respond(embed=res)
         elif isinstance(res, str):
-            await ctx.send(res)
+            await ctx.respond(res)
 
-    @commands.command(aliases=['i'])
+    @bridge.bridge_command(aliases=['i'])
     async def info(self, ctx, name: str = None):
         """View Hypixel stats of the given user!"""
         if not name:
             name = await name_grabber(ctx.author)
-        await ctx.send(embed=await String(string=name).info())
+        await ctx.respond(embed=await String(string=name).info())
 
-    @commands.command(aliases=['dnkladd'])
-    @commands.has_permissions(manage_messages=True)
-    async def Dnkl_Add(self, ctx, name: str):
+    @bridge.bridge_command(aliases=['dnkladd', 'dnkla'])
+    @commands.has_any_role("Staff")
+    async def dnkl_add(self, ctx, name: str):
         """Add a user to the do-not-kick-list!"""
         res = await String(string=name).dnkladd(ctx)
         if isinstance(res, str):
-            await ctx.send(res)
+            await ctx.respond(res)
         elif isinstance(res, discord.Embed):
-            await ctx.send(embed=res)
+            await ctx.respond(embed=res)
 
-    @commands.command(aliases=['dnklrmv'])
+    @bridge.bridge_command(aliases=['dnklrmv', 'dnklremove', 'dnklremv', 'dnklr'])
     @commands.has_permissions(manage_messages=True)
-    async def DNKL_Remove(self, ctx, name: str):
+    async def dnkl_remove(self, ctx, name: str):
         """Remove a player from the do-not-kick-list"""
-        await ctx.send(await String(string=name).dnklremove())
+        await ctx.respond(await String(string=name).dnklremove())
 
-    @commands.command(aliases=['dnkllist'])
-    async def DNKL_List(self, ctx):
+    @bridge.bridge_command(aliases=['dnkllist', 'dnkll'])
+    async def dnkl_list(self, ctx):
         """View all users on the do-not-kick-list!"""
-        await ctx.send(embed=await General.dnkllist(ctx))
+        await ctx.respond(embed=await General.dnkllist(ctx))
 
-    @commands.command(aliases=['dnklchk'])
-    async def DNKL_Check(self, ctx, name: str = None):
+    @bridge.bridge_command(aliases=['dnklchk', 'dnklcheck', 'dnklc'])
+    async def dnkl_check(self, ctx, name: str = None):
         """Check whether you are eligible for the do-not-kick-list!"""
         if not name:
             name = await name_grabber(ctx.author)
 
         res = await String(string=name).dnklcheck()
         if isinstance(res, discord.Embed):
-            await ctx.send(embed=res)
+            await ctx.respond(embed=res)
         elif isinstance(res, str):
-            await ctx.send(res)
+            await ctx.respond(res)
 
 
 def setup(bot):
