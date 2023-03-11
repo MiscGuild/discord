f7c952189503456ffb6b1c9d7ad984a77b10af1d
Amogh <86120332+Amxgh@users.noreply.github.com>
2023-03-11 04:47:25 -0600

    refactor: gmember

--- src/func/String.py
+++ src/func/String.py
@@ -64,6 +64,9 @@ class String:
 
     async def gmember(self, ctx):
         name, uuid = await get_mojang_profile(self.string)
+        if not name:
+            return unknown_ign_embed
+        
         guild = await get_player_guild(uuid)
 
         # Player is guildless
@@ -72,57 +75,79 @@ class String:
 
         # Get guild data
         gname = guild["name"]
-        gtag = gname if not "tag" in guild else guild["tag"]
+        gtag = "" if not "tag" in guild else f"[{guild['tag']}]"
 
         # Find player in req
         for member in guild["members"]:
-            if member["uuid"] == uuid:
-                # Get player data
-                gexp_history = member["expHistory"]
-                weekly_gexp = sum(gexp_history.values())
+            if member["uuid"] != uuid:
+                continue
 
-                # Send shortened version for non-command channels
-                if "commands" not in ctx.channel.name:
-                    return f"__**{name}**__\n**Guild Experience -** `{format(weekly_gexp, ',d')}`"
 
-                week_dict = {0: "Today:", 1: "Yesterday:", 2: "Two days ago:", 3: "Three days ago:",
-                             4: "Four days ago:", 5: "Five days ago:", 6: "Six days ago:"}
+            # Get player data
+            gexp_history = member["expHistory"]
+            weekly_gexp = sum(gexp_history.values())
 
-                # Fetch remaining data
-                join_date = str(datetime.fromtimestamp(int(str(member["joined"])[:-3])))[:10]
-                rank = member["rank"]
-                quest_participation = member["questParticipation"] if "questParticipation" in member else 0
-                dates = [k for k, _ in gexp_history.items()]
-                gexp_vals = [int(v) for _, v in gexp_history.items()]
-                gexp_history_text = ""
-                for i in range(0, 7):
-                    date = week_dict.get(i, "None")
-                    gexp_history_text = gexp_history_text + f"**▸** {date} **{format(gexp_vals[i], ',d')}**\n"
+            # Send shortened version for non-command channels
+            if "commands" not in ctx.channel.name:
+                return f"__**{name}**__\n**Guild Experience -** `{format(weekly_gexp, ',d')}`"
 
-                # Get graph color
-                color, graph_color, graph_border = await get_color_by_gexp(rank, weekly_gexp)
+            week_dict = {
+                        0: "Today:",
+                        1: "Yesterday:", 
+                        2: "Two days ago:", 
+                        3: "Three days ago:",
+                        4: "Four days ago:", 
+                        5: "Five days ago:", 
+                        6: "Six days ago:"
+                        }
 
-                # Create embed
-                embed = discord.Embed(title=name, url=f"https://plancke.io/hypixel/player/stats/{name}", color=color)
-                embed.set_author(name=f"{gname} [{gtag}]", url=f"https://plancke.io/hypixel/guild/player/{name}")
-                embed.set_thumbnail(url=f"https://minotar.net/helm/{uuid}/512.png")
-                embed.add_field(name="General Information:", value=f"`✚` **Rank**: `{rank}`\n"
-                                                                   f"`✚` **Joined**: `{join_date}`\n"
-                                                                   f"`✚` **Quests Completed**: `{quest_participation}`\n"
-                                                                   f"`✚` **Overall Guild Experience**: `{format(weekly_gexp, ',d')}`\n\n{gexp_history_text}",
-                                inline=False)
+            # Fetch remaining data
+            join_date = str(datetime.fromtimestamp(int(str(member["joined"])[:-3])))[:10]
+            rank = member["rank"]
+            quest_participation = member["questParticipation"] if "questParticipation" in member else 0
+            dates = [k for k, _ in gexp_history.items()]
+            gexp_vals = [int(v) for _, v in gexp_history.items()]
+            gexp_history_text = ""
+            for i in range(0, 7):
+                date = week_dict.get(i, "None")
+                gexp_history_text = gexp_history_text + f"**▸** {date} **{format(gexp_vals[i], ',d')}**\n"
 
-                # Create chart
-                dates.reverse()
-                gexp_vals.reverse()
-                chart = QuickChart()
-                chart.width = 1000
-                chart.height = 500
-                chart.background_color = "transparent"
-                chart.config = {"type": "line", "data": {"labels": dates, "datasets": [
-                    {"label": "Experience", "data": gexp_vals, "lineTension": 0.4, "backgroundColor": graph_color,
-                     "borderColor": graph_border, "pointRadius": 0, }]}}
-                return embed.set_image(url=chart.get_url())
+            # Get graph color
+            color, graph_color, graph_border = await get_color_by_gexp(rank, weekly_gexp)
+
+            # Create embed
+            embed = discord.Embed(title=name, url=f"https://plancke.io/hypixel/player/stats/{name}", color=color)
+            embed.set_author(name=f"{gname} {gtag}", url=f"https://plancke.io/hypixel/guild/player/{name}")
+            embed.set_thumbnail(url=f"https://minotar.net/helm/{uuid}/512.png")
+            embed.add_field(name="General Information:", value=f"`✚` **Rank**: `{rank}`\n"
+                                                                f"`✚` **Joined**: `{join_date}`\n"
+                                                                f"`✚` **Quests Completed**: `{quest_participation}`\n"
+                                                                f"`✚` **Overall Guild Experience**: `{format(weekly_gexp, ',d')}`\n\n{gexp_history_text}",
+                            inline=False)
+
+            # Create chart
+            dates.reverse()
+            gexp_vals.reverse()
+            chart = QuickChart()
+            chart.width = 1000
+            chart.height = 500
+            chart.background_color = "transparent"
+            chart.config = {"type": "line", 
+                            "data": {
+                                    "labels": dates,
+                                    "datasets": [
+                                                {
+                                                "label": "Experience",
+                                                "data": gexp_vals, 
+                                                "lineTension": 0.4, 
+                                                "backgroundColor": graph_color,
+                                                "borderColor": graph_border,
+                                                "pointRadius": 0, 
+                                                }
+                                                ]
+                                    }
+                            }
+            return embed.set_image(url=chart.get_url())
 
     async def info(self):
         player_data = await get_hypixel_player(name=self.string)
