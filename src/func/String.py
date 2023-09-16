3203151fa0de69b7ee76b955909e298b2b70379d
Amogh <axm0754@mavs.uta.edu>
2023-09-16 14:19:26 -0500

    fix: added support for people who have 0 invites

--- src/func/String.py
+++ src/func/String.py
@@ -287,15 +287,18 @@ class String:
         guild = await get_player_guild(uuid)
         if guild["name"] != guild_handle:
             return missing_permissions_embed
-        weekly_invites, total_invites, total_valid_invites = await get_invites(uuid)
-
-        weekly_invites = weekly_invites.split()
-        weekly_invites = [await get_name_by_uuid(invitee) for invitee in weekly_invites]
-        invites = ""
+        invites = await get_invites(uuid)
+        invites_text = ""
+        if not invites:
+            weekly_invites, total_invites, total_valid_invites = None, "0", "0"
+        else:
+            weekly_invites, total_invites, total_valid_invites = invites
+            weekly_invites = weekly_invites.split()
+            weekly_invites = [await get_name_by_uuid(invitee) for invitee in weekly_invites]
+            for invitee in weekly_invites:
+                invites_text += f"**▸** {invitee}\n"
         embed = discord.Embed(title=f"{ign}'s Invites", color=neutral_color)
-        for invitee in weekly_invites:
-            invites += f"**▸** {invitee}\n"
-        embed.add_field(name="Weekly Invites", value=invites, inline=False)
+        embed.add_field(name="Weekly Invites", value=None if not invites_text else invites_text, inline=False)
         embed.add_field(name="Total Invites", value=total_invites, inline=True)
         embed.add_field(name="Total Valid Invites", value=total_valid_invites, inline=True)
         embed.set_footer(text="Total invites and total valid invites do not include this week's invites. They are "
