5c03998508393d3e7a6f8628c075ec271d0dd121
Amogh <axm0754@mavs.uta.edu>
2023-10-20 11:20:09 -0500

    fix: typo fix

--- src/utils/referral_utils.py
+++ src/utils/referral_utils.py
@@ -102,7 +102,7 @@ async def generate_rank_upgrade(weekly_invites : list):
 **The winner is....**
 ## {winner}
 > Total Guild Experience:- `{format(winner_gexp, ',d')}`
-> Valid Invites:- `{format(len(winner_invites), ',d')}`
+> Valid Invites:- `{format(len(winner_invites), ',d') if winner_invites else 0}`
 > Total Entries:- `{format(entries[winner_uuid], ',d') if winner_uuid in entries else 0}`
 
 
