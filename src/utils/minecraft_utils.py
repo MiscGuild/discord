dce7166e77a4ae3d2319878f2ade9261aeb9780c
suspendedtundra2@gmail.com <axm0754@mavs.uta.edu>
2024-01-25 02:18:56 -0600

    refactor: moved get_guild_level() from request_utils.py to minecraft_utils.py

--- src/utils/minecraft_utils.py
+++ src/utils/minecraft_utils.py
@@ -139,3 +139,32 @@ async def generate_lb_text(member_gexp: list, text: str):
 
     # Return image
     return text
+
+
+async def get_guild_level(exp):
+    EXP_NEEDED = [100000, 150000, 250000, 500000, 750000, 1000000, 1250000, 1500000, 2000000, 2500000, 2500000, 2500000,
+                  2500000, 2500000, 3000000]
+    # A list of amount of XP required for leveling up in each of the beginning levels (1-15).
+
+    level = 0
+
+    for i in range(1000):
+        # Increment by one from zero to the level cap.
+        need = 0
+        if i >= len(EXP_NEEDED):
+            need = EXP_NEEDED[len(EXP_NEEDED) - 1]
+        else:
+            need = EXP_NEEDED[i]
+        # Determine the current amount of XP required to level up,
+        # in regards to the "i" variable.
+
+        if (exp - need) < 0:
+            return round(((level + (exp / need)) * 100) / 100, 2)
+        # If the remaining exp < the total amount of XP required for the next level,
+        # return their level using this formula.
+
+        level += 1
+        exp -= need
+        # Otherwise, increase their level by one,
+        # and subtract the required amount of XP to level up,
+        # from the total amount of XP that the guild had.
