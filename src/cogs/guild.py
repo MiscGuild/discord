0b29c4c297375f7c0919ecefd4797a74e74d1724
suspendedtundra2@gmail.com <axm0754@mavs.uta.edu>
2024-03-21 03:26:07 -0500

    feat: invites() now fetches the uuid and username from the db instead of looking at a user's nick.

--- src/cogs/guild.py
+++ src/cogs/guild.py
@@ -6,7 +6,7 @@ from src.func.General import General
 from src.func.Integer import Integer
 from src.func.String import String
 from src.utils.consts import gvg_info_embed, requirements_embed, resident_embed
-from src.utils.discord_utils import name_grabber
+from src.utils.db_utils import get_db_uuid_username_from_discord_id
 
 
 class Guild(commands.Cog, name="guild"):
@@ -92,8 +92,11 @@ class Guild(commands.Cog, name="guild"):
         """View your invitation stats"""
         await ctx.defer()
         if not name:
-            name = await name_grabber(ctx.author)
-        await ctx.respond(embed=await String(name).invites())
+            uuid, _ = await get_db_uuid_username_from_discord_id(ctx.author.id)
+            res = await String(string=uuid).invites()
+        else:
+            res = await String(string=name).invites()
+        await ctx.respond(embed=res)
 
 def setup(bot):
     bot.add_cog(Guild(bot))
