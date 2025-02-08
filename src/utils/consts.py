c6637a0203411b8598c58ccb67d800697e55b107
suspendedtundra2@gmail.com <axm0754@mavs.uta.edu>
2025-02-08 14:58:41 -0600

    refactor: Update rainbow_separator to use a secure HTTPS protocol

--- src/utils/consts.py
+++ src/utils/consts.py
@@ -362,7 +362,7 @@ This rank upgrade must be claimed within a week of this message by creating a ti
 
 *To know how the winner is picked, go here https://discord.com/channels/522586672148381726/1152480866585554994/1164962591198683146*'''
 
-rainbow_separator = "http://rainbowdivider.com/images/dividers/movrblin02.gif"
+rainbow_separator = "https://rainbowdivider.com/images/dividers/movrblin02.gif"
 
 
 class ChatColor(Enum):
@@ -412,44 +412,3 @@ staff_application_questions = {
     12: "Tell us about a time you made a mistake within the last year. How did you deal with it? What did you learn?",
     13: "Anything else you like would us to know?"
 }
-
-
-async def reactionroles() -> tuple[list, list]:
-    # Reaction roles
-    reaction_roles_embed = discord.Embed(title="To get your desired role, click its respective button!",
-                                         description="🪓 __**SkyBlock**__\nGives you the access to the SkyBlock category!\n\n"
-                                                     "🕹 __**Minigames**__\nAllows you to play some Discord minigames!\n\n"
-                                                     "❓  __**QOTD Ping**__\nThe staff team will mention this role when there's a new question of the day!\n\n"
-                                                     "🎉 __**Giveaways/Events**__\nReact so you don't miss any giveaway or event\n\n"
-                                                     "📖 __**Storytime Pings**__\nGet pinged whenever a storytime happens",
-                                         color=neutral_color)
-
-    class ReactionRoleButton(Button):
-        def __init__(self, label: str, emoji: str):
-            super().__init__(label=label, custom_id=label, emoji=emoji)
-
-    class ReactionRolesView(View):
-        def __init__(self):
-            super().__init__()
-
-            # Add all buttons
-            for k, v, in reaction_roles.items():
-                self.add_item(ReactionRoleButton(k, v))
-
-    # Pronouns
-    pronouns_embed = discord.Embed(title="Please select your pronouns",
-                                   description="".join(
-                                       [k + v + "\n" for k, v in pronoun_roles.items()]),
-                                   color=neutral_color)
-
-    class PronounsSelect(Select):
-        def __init__(self):
-            options = [discord.SelectOption(
-                label=k, emoji=v) for k, v in pronoun_roles.items()]
-            super().__init__(placeholder="Select your pronouns (Max 1)",
-                             min_values=0, max_values=1, options=options, custom_id="pronouns")
-
-    pronouns_view = View(timeout=10.0)
-    pronouns_view.add_item(PronounsSelect())
-
-    return [reaction_roles_embed, ReactionRolesView()], [pronouns_embed, pronouns_view]
