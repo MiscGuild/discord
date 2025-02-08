7de1b4fd746b5681c417d92717a7fa949477b01f
suspendedtundra2@gmail.com <axm0754@mavs.uta.edu>
2025-02-08 14:37:04 -0600

    refactor: Use UTC for end date calculation in duration handling logic in General.py

--- src/func/General.py
+++ src/func/General.py
@@ -3,7 +3,7 @@
 import asyncio
 import re
 from __main__ import bot
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
 
 import discord
 import discord.ui
@@ -492,7 +492,7 @@ class General:
             if duration == "cancel":
                 return "Giveaway cancelled!"
             try:
-                end_date = datetime.utcnow() + timedelta(seconds=duration)
+                end_date = datetime.now(UTC) + timedelta(seconds=duration)
                 end_date = end_date.strftime("%Y-%m-%d %H:%M:%S.%f")[:-7]
             except (TypeError, ValueError):
                 await ctx.send("Invalid duration! Please try again.", delete_after=3)
