9ac5a8db21fd960d345f7af6a0e812a063e86f5a
suspendedtundra2@gmail.com <axm0754@mavs.uta.edu>
2025-02-07 23:46:59 -0600

    refactor: Add return type hints for sync, info, dnkl_add, dnkl_remove, dnkl_list, and dnkl_check command functions

--- src/cogs/hypixel.py
+++ src/cogs/hypixel.py
@@ -29,7 +29,7 @@ class Hypixel(commands.Cog, name="hypixel"):
         required=False,
         input_type=str
     )
-    async def sync(self, ctx: Context, name, tag: str = None):
+    async def sync(self, ctx: Context, name, tag: str = None) -> None:
         """Update your discord nick, tag and roles!"""
         res = await Union(user=ctx.author).sync(ctx, name, tag)
         if isinstance(res, discord.Embed):
@@ -44,7 +44,7 @@ class Hypixel(commands.Cog, name="hypixel"):
         required=False,
         input_type=str
     )
-    async def info(self, ctx: Context, name: str = None):
+    async def info(self, ctx: Context, name: str = None) -> None:
         """View Hypixel stats of the given user!"""
         if not name:
             uuid, username = await get_db_uuid_username_from_discord_id(ctx.author.id)
@@ -61,7 +61,7 @@ class Hypixel(commands.Cog, name="hypixel"):
         required=True,
         input_type=str
     )
-    async def dnkl_add(self, ctx: Context, name: str):
+    async def dnkl_add(self, ctx: Context, name: str) -> None:
         """Add a user to the do-not-kick-list!"""
         res = await String(string=name).dnkladd(ctx)
         if isinstance(res, str):
@@ -83,7 +83,7 @@ class Hypixel(commands.Cog, name="hypixel"):
         required=False,
         input_type=str
     )
-    async def dnkl_remove(self, ctx: Context, name: str = None, uuid: str = None):
+    async def dnkl_remove(self, ctx: Context, name: str = None, uuid: str = None) -> None:
         """Remove a player from the do-not-kick-list"""
         if not name and not uuid:
             await ctx.respond("Please provide either the username or the UUID of the player you want to remove.")
@@ -96,7 +96,7 @@ class Hypixel(commands.Cog, name="hypixel"):
             await ctx.respond(await String(string=name).dnklremove())
 
     @bridge.bridge_command(aliases=['dnkllist', 'dnkll'])
-    async def dnkl_list(self, ctx):
+    async def dnkl_list(self, ctx: Context) -> None:
         """View all users on the do-not-kick-list!"""
         await ctx.respond(embed=await General().dnkllist(ctx))
 
@@ -107,7 +107,7 @@ class Hypixel(commands.Cog, name="hypixel"):
         required=False,
         input_type=str
     )
-    async def dnkl_check(self, ctx: Context, name: str = None):
+    async def dnkl_check(self, ctx: Context, name: str = None) -> None:
         """Check whether you are eligible for the do-not-kick-list!"""
         if not name:
             uuid, username = await get_db_uuid_username_from_discord_id(ctx.author.id)
