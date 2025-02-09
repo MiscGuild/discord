bf8bc9115f735d7ec4a55a75b4513743228a787a
suspendedtundra2@gmail.com <axm0754@mavs.uta.edu>
2025-02-09 09:32:15 -0600

    feat: database stores the 365 most recent gexp entries

--- src/utils/db_utils.py
+++ src/utils/db_utils.py
@@ -188,14 +188,21 @@ async def get_member_gexp_history(uuid: str) -> dict:
 
 async def set_member_gexp_history(uuid: str, gexp_history: dict) -> dict:
     current_history: dict = await get_member_gexp_history(uuid)
+
     if not current_history:
+        # Direct insert if no existing history
         await bot.db.execute("INSERT INTO guild_member_data VALUES (?, ?)", (uuid, json.dumps(gexp_history)))
         await bot.db.commit()
         return gexp_history
+
     current_history.update(gexp_history)
 
-    await bot.db.execute("UPDATE guild_member_data set gexp_history = ? where uuid = ?",
-                         (json.dumps(current_history), uuid))
+    sorted_history = dict(sorted(current_history.items(), reverse=True))
+
+    limited_history = dict(list(sorted_history.items())[:365])  # Keeps the 365 most recent entries
+
+    await bot.db.execute("UPDATE guild_member_data SET gexp_history = ? WHERE uuid = ?",
+                         (json.dumps(limited_history), uuid))
     await bot.db.commit()
 
-    return current_history
+    return limited_history
