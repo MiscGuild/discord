e39cb8eb4f18b835c83733aa574903425a5d5744
suspendedtundra2@gmail.com <axm0754@mavs.uta.edu>
2025-05-27 03:50:11 +0400

    feat: add user parameter to player_report function for improved user context

--- src/utils/ticket_utils/player_report.py
+++ src/utils/ticket_utils/player_report.py
@@ -4,10 +4,8 @@ import src.utils.ui_utils as uiutils
 from src.utils.consts import neutral_color, ticket_categories
 
 
-async def player_report(ticket: discord.TextChannel, interaction: discord.Interaction, ign: str, uuid: str):
-    await ticket.edit(name=f"report-{ign}", topic=f"{interaction.user.id}|",
-                      category=discord.utils.get(interaction.guild.categories,
-                                                 name=ticket_categories["generic"]))
+async def player_report(ticket: discord.TextChannel, interaction: discord.Interaction, user: discord.Member, ign: str,
+                        uuid: str):
     fields = [
         ["What was the username of the accused", "", discord.InputTextStyle.short,
          "Username of the accused"],
