e44e0603a0dd54bd213fa62046481ff9affe37d7
suspendedtundra2@gmail.com <axm0754@mavs.uta.edu>
2025-05-27 03:50:55 +0400

    feat: update milestone function to include user parameter for improved context

--- src/utils/ticket_utils/milestone.py
+++ src/utils/ticket_utils/milestone.py
@@ -3,9 +3,9 @@ import discord
 from src.utils.consts import neutral_color, ticket_categories
 
 
-async def milestone(ticket: discord.TextChannel, interaction: discord.Interaction, ign: str):
-    await ticket.edit(name=f"milestone-{ign}", topic=f"{interaction.user.id}|",
-                      category=discord.utils.get(interaction.guild.categories,
+async def milestone(ticket: discord.TextChannel, interaction: discord.Interaction, user: discord.Member, ign: str):
+    await ticket.edit(name=f"milestone-{ign}", topic=f"{interaction.user.id if interaction else user.id}|",
+                      category=discord.utils.get((interaction.guild if interaction else ticket.guild).categories,
                                                  name=ticket_categories[
                                                      "milestone"]))
     await ticket.send(embed=discord.Embed(title=f"{ign} would like to register a milestone!",
