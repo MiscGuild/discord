e0cd6c4f2c7ca21926338a805e4c3ab7759f9f93
suspendedtundra2@gmail.com <axm0754@mavs.uta.edu>
2025-05-27 03:50:43 +0400

    feat: update organize_gvg function to include user parameter for improved context

--- src/utils/ticket_utils/organize_gvg.py
+++ src/utils/ticket_utils/organize_gvg.py
@@ -2,14 +2,12 @@ import discord
 
 import src.utils.ui_utils as uiutils
 from src.utils.calculation_utils import get_guild_level
-from src.utils.consts import neutral_color, ticket_categories
+from src.utils.consts import neutral_color, ticket_categories, guild_handle
 from src.utils.request_utils import get_player_guild
 
 
-async def organize_gvg(ticket: discord.TextChannel, interaction: discord.Interaction, ign: str, uuid: str):
-    await ticket.edit(name=f"gvg-request-{ign}", topic=f"{interaction.user.id}|",
-                      category=discord.utils.get(interaction.guild.categories,
-                                                 name=ticket_categories["generic"]))
+async def organize_gvg(ticket: discord.TextChannel, interaction: discord.Interaction, user: discord.Member, ign: str,
+                       uuid: str):
     guild = await get_player_guild(uuid)
     fields = []
     if not guild:
