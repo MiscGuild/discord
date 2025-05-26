d2dff5b2969668833c412843ae6b3fe8c6cd7e1c
suspendedtundra2@gmail.com <axm0754@mavs.uta.edu>
2025-05-27 03:51:03 +0400

    feat: update milestone function to include user parameter for improved context

--- src/utils/ticket_utils/join_guild.py
+++ src/utils/ticket_utils/join_guild.py
@@ -5,14 +5,13 @@ import discord
 from src.utils.consts import neutral_color, ticket_categories
 
 
-async def join_guild(ticket: discord.TextChannel, interaction: discord.Interaction, ign: str):
+async def join_guild(ticket: discord.TextChannel, interaction: discord.Interaction, user: discord.Member, ign: str):
     # Edit category and send info embed with requirements
-    await ticket.edit(name=f"join-request-{ign}", topic=f"{interaction.user.id}|",
-                      category=discord.utils.get(interaction.guild.categories,
+    await ticket.edit(name=f"join-request-{ign}", topic=f"{interaction.user.id if interaction else user.id}|",
+                      category=discord.utils.get((interaction.guild if interaction else ticket.guild).categories,
                                                  name=ticket_categories["registrees"]))
     await ticket.purge(limit=100)
     await ticket.send(
         embed=discord.Embed(title=f"{ign} wishes to join Miscellaneous!",
                             description=f"Please await staff assistance!\nIn the meanwhile, you may explore the Discord!",
                             color=neutral_color))
-    await interaction.user.add_roles(bot.guest, reason="Registration - Guest")
