f75bf069f562204c5ba4b7a2586de418b223ebef
suspendedtundra2@gmail.com <amogh@amxgh.com>
2026-01-21 20:23:11 -0600

    refactor: update rank_upgrade.py to use constants for color and ticket categories

--- src/utils/ticket_utils/rank_upgrade.py
+++ src/utils/ticket_utils/rank_upgrade.py
@@ -1,14 +1,14 @@
 import discord
 
-from src.utils.consts import neutral_color, ticket_categories
+from src.utils.consts import NEUTRAL_COLOR, TICKET_CATEGORIES
 
 
 async def rank_upgrade(ticket: discord.TextChannel, interaction: discord.Interaction, user: discord.Member, ign: str):
     await ticket.edit(name=f"rank-upgrade-{ign}", topic=f"{interaction.user.id if interaction else user.id}|",
                       category=discord.utils.get((interaction.guild if interaction else ticket.guild).categories,
-                                                 name=ticket_categories[
+                                                 name=TICKET_CATEGORIES[
                                                      "generic"]))
     await ticket.send(embed=discord.Embed(title=f"{ign} claims to have won a rank upgrade.",
                                           description="Please provide a link to a message saying you won a rank upgrade.",
-                                          color=neutral_color).set_footer(
+                                          color=NEUTRAL_COLOR).set_footer(
         text="You might have to wait up to a month to receive your rank upgrade!"))
