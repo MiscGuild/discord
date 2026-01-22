76bde5e42ef65cf50f39e3b84ef82ae9460ab989
suspendedtundra2@gmail.com <amogh@amxgh.com>
2026-01-21 20:21:18 -0600

    refactor: update dnkl.py to use constants for embeds and colors

--- src/utils/ticket_utils/dnkl.py
+++ src/utils/ticket_utils/dnkl.py
@@ -4,8 +4,8 @@ import discord
 
 import src.utils.ui_utils as uiutils
 from src.utils.calculation_utils import get_player_gexp
-from src.utils.consts import neutral_color, ticket_categories, neg_color, unknown_ign_embed, dnkl_creation_embed, \
-    dnkl_req, missing_permissions_embed, dnkl_channel_id
+from src.utils.consts import NEUTRAL_COLOR, TICKET_CATEGORIES, NEG_COLOR, UNKNOWN_IGN_EMBED, DNKL_CREATION_EMBED, \
+    DNKL_REQ, MISSING_PERMS_EMBED, DNKL_CHANNEL_ID
 from src.utils.db_utils import insert_new_dnkl, update_dnkl, select_one, delete_dnkl
 from src.utils.ticket_utils.tickets import close_ticket
 
@@ -20,27 +20,27 @@ async def dnkl_application(ign: str, uuid: str, channel: discord.TextChannel, au
                                               weekly_gexp=weekly_gexp, days_in_guild=days_in_guild,
                                               buttons=buttons))  # Year Selection Dropdown
     embed = discord.Embed(title=f"In which year will {ign}'s inactivity begin?",
-                          color=neutral_color)
+                          color=NEUTRAL_COLOR)
     await channel.send(embed=embed, view=YearView)
 
 
 async def dnkl_error(channel: discord.TextChannel, author: discord.User, ign: str, uuid: str, embed: discord.Embed,
                      interaction: discord.Interaction):
     if bot.staff not in interaction.user.roles:
-        await channel.send(embed=missing_permissions_embed)
+        await channel.send(embed=MISSING_PERMS_EMBED)
         return None
 
     await interaction.response.send_message(embed=discord.Embed(
         title="Your application has been accepted, however there was an error!",
         description="Please await staff assistance!",
-        color=neutral_color))
+        color=NEUTRAL_COLOR))
     return True
 
 
 async def dnkl_deny(channel: discord.TextChannel, author: discord.User, ign: str, uuid: str, embed: discord.Embed,
                     interaction: discord.Interaction, self_denial: bool = False):
     if bot.staff not in interaction.user.roles and not self_denial:
-        await channel.send(embed=missing_permissions_embed)
+        await channel.send(embed=MISSING_PERMS_EMBED)
         return None
 
     if not self_denial:
@@ -49,8 +49,8 @@ async def dnkl_deny(channel: discord.TextChannel, author: discord.User, ign: str
                                                 ephemeral=True)
 
         embed = discord.Embed(title="Your do-not-kick-list application has been denied!",
-                              description=f"You do not meet the DNKL requirements of {format(dnkl_req, ',d')} weekly guild experience.",
-                              color=neg_color)
+                              description=f"You do not meet the DNKL requirements of {format(DNKL_REQ, ',d')} weekly guild experience.",
+                              color=NEG_COLOR)
         embed.set_footer(
             text="If don't you think you can meet the requirements, you may rejoin the guild once your inactivity period has ended.")
 
@@ -68,10 +68,10 @@ async def dnkl_deny(channel: discord.TextChannel, author: discord.User, ign: str
 async def dnkl_approve(channel: discord.TextChannel, author: discord.User, ign: str, uuid: str, embed: discord.Embed,
                        interaction: discord.Interaction):
     if bot.staff not in interaction.user.roles:
-        await channel.send(embed=missing_permissions_embed)
+        await channel.send(embed=MISSING_PERMS_EMBED)
         return None
     message = await interaction.response.send_message("Processing...")
-    msg = await bot.get_channel(dnkl_channel_id).send(embed=embed)
+    msg = await bot.get_channel(DNKL_CHANNEL_ID).send(embed=embed)
 
     # Check if user is already on DNKL
     current_message = (await select_one("SELECT message_id FROM dnkl WHERE uuid = (?)",
@@ -87,7 +87,7 @@ async def dnkl_approve(channel: discord.TextChannel, author: discord.User, ign: 
     # User is already on DNKl
     # Try to delete current message
     try:
-        current_message = await bot.get_channel(dnkl_channel_id).fetch_message(
+        current_message = await bot.get_channel(DNKL_CHANNEL_ID).fetch_message(
             current_message)
         await current_message.delete()
     except discord.NotFound:
@@ -105,23 +105,23 @@ async def dnkl(ticket: discord.TextChannel, interaction: discord.Interaction, us
     # Edit channel name and category
     await ticket.edit(name=f"dnkl-{ign}", topic=f"{interaction.user.id if interaction else user.id}|",
                       category=discord.utils.get((interaction.guild if interaction else ticket.guild).categories,
-                                                 name=ticket_categories["dnkl"]))
+                                                 name=TICKET_CATEGORIES["dnkl"]))
 
     # Notify user if they don't meet gexp req, however ask questions anyway
     _, weekly_gexp, days_in_guild = await get_player_gexp(uuid)
     if weekly_gexp is None:
-        return await ticket.send(embed=unknown_ign_embed)
-    await ticket.send(embed=dnkl_creation_embed)
-    if weekly_gexp < dnkl_req:
+        return await ticket.send(embed=UNKNOWN_IGN_EMBED)
+    await ticket.send(embed=DNKL_CREATION_EMBED)
+    if weekly_gexp < DNKL_REQ:
         await ticket.send(
             embed=discord.Embed(title="You do not meet the do-not-kick-list requirements!",
                                 description=f"Even though you do not meet the requirements, "
                                             f"your application may still be accepted.\n"
                                             f"You have {format(weekly_gexp, ',d')} weekly guild experience!",
-                                color=neg_color))
+                                color=NEG_COLOR))
     else:
         await ticket.send(embed=discord.Embed(title="You meet the do-not-kick-list requirements!",
                                               description=f"You have {format(weekly_gexp, ',d')} weekly guild experience!",
-                                              color=neutral_color))
+                                              color=NEUTRAL_COLOR))
 
     await dnkl_application(ign, uuid, ticket, (interaction.user if interaction else user), weekly_gexp, days_in_guild)
