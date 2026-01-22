7299bf009b9ef957b8c81c2cd1588ffd72cafe37
suspendedtundra2@gmail.com <amogh@amxgh.com>
2026-01-21 20:22:23 -0600

    refactor: update join_guild.py to use constants for ticket categories and embed

--- src/utils/ticket_utils/join_guild.py
+++ src/utils/ticket_utils/join_guild.py
@@ -2,14 +2,14 @@ from __main__ import bot
 
 import discord
 
-from src.utils.consts import ticket_categories, join_request_embed
+from src.utils.consts import TICKET_CATEGORIES, JOIN_REQUEST_EMBED
 
 
 async def join_guild(ticket: discord.TextChannel, interaction: discord.Interaction, user: discord.Member, ign: str):
     # Edit category and send info embed with requirements
     await ticket.edit(name=f"join-request-{ign}", topic=f"{interaction.user.id if interaction else user.id}|",
                       category=discord.utils.get((interaction.guild if interaction else ticket.guild).categories,
-                                                 name=ticket_categories["registrees"]))
+                                                 name=TICKET_CATEGORIES["registrees"]))
     await ticket.purge(limit=100)
     await ticket.send(
-        embed=join_request_embed.set_author(name=f"{ign} wishes to join Miscellaneous"))
+        embed=JOIN_REQUEST_EMBED.set_author(name=f"{ign} wishes to join Miscellaneous"))
