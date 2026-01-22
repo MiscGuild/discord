9097215cdc887e5facaa4cbe536706315927a5dd
suspendedtundra2@gmail.com <amogh@amxgh.com>
2026-01-21 20:22:41 -0600

    refactor: update milestone.py to use constants for color and ticket categories

--- src/utils/ticket_utils/milestone.py
+++ src/utils/ticket_utils/milestone.py
@@ -1,13 +1,13 @@
 import discord
 
-from src.utils.consts import neutral_color, ticket_categories
+from src.utils.consts import NEUTRAL_COLOR, TICKET_CATEGORIES
 
 
 async def milestone(ticket: discord.TextChannel, interaction: discord.Interaction, user: discord.Member, ign: str):
     await ticket.edit(name=f"milestone-{ign}", topic=f"{interaction.user.id if interaction else user.id}|",
                       category=discord.utils.get((interaction.guild if interaction else ticket.guild).categories,
-                                                 name=ticket_categories[
+                                                 name=TICKET_CATEGORIES[
                                                      "milestone"]))
     await ticket.send(embed=discord.Embed(title=f"{ign} would like to register a milestone!",
                                           description="Please provide a small description and proof of your milestone!\nIf your milestone is approved, it'll be included in next week's milestone post!",
-                                          color=neutral_color))
+                                          color=NEUTRAL_COLOR))
