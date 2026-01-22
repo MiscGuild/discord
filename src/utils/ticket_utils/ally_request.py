0903092e2e32cea91b50303c7d873a9a20bd86e7
suspendedtundra2@gmail.com <amogh@amxgh.com>
2026-01-21 20:20:16 -0600

    feat: update ally_request.py to use constants from consts module

--- src/utils/ticket_utils/ally_request.py
+++ src/utils/ticket_utils/ally_request.py
@@ -2,7 +2,7 @@ import discord
 
 import src.utils.ui_utils as uiutils
 from src.utils.calculation_utils import get_guild_level
-from src.utils.consts import neutral_color, ticket_categories, guild_handle
+from src.utils.consts import NEUTRAL_COLOR, TICKET_CATEGORIES, GUILD_HANDLE
 from src.utils.request_utils import get_player_guild
 
 
@@ -15,12 +15,12 @@ async def ally_request(ticket: discord.TextChannel, interaction: discord.Interac
         fields.extend(
             [["What is the name of your guild?", "", discord.InputTextStyle.short, "Guild Name"],
              ["What is your guild's level?", "", discord.InputTextStyle.short, "Guild Level"]])
-        embed = discord.Embed(title="Alliance Request Request", color=neutral_color)
+        embed = discord.Embed(title="Alliance Request Request", color=NEUTRAL_COLOR)
     else:
         embed = discord.Embed(
-            title=f"{ign} wishes to ally with {guild_handle} on behalf of {guild['name']}",
+            title=f"{ign} wishes to ally with {GUILD_HANDLE} on behalf of {guild['name']}",
             description=f"Guild Level: {await get_guild_level(guild['exp'])}",
-            color=neutral_color)
+            color=NEUTRAL_COLOR)
     embed.set_footer(text="Please provide:\nGuild Logo\nGuild Advertisement Message")
     fields.extend(
         [["What is the IGN of your guild master?", "", discord.InputTextStyle.short, "Guild Master"],
@@ -34,4 +34,4 @@ async def ally_request(ticket: discord.TextChannel, interaction: discord.Interac
 
     await ticket.edit(name=f"alliance-request-{ign}", topic=f"{interaction.user.id if interaction else user.id}|",
                       category=discord.utils.get((interaction.guild if interaction else ticket.guild).categories,
-                                                 name=ticket_categories["generic"]))
+                                                 name=TICKET_CATEGORIES["generic"]))
