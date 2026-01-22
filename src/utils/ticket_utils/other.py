fc04ee8186dc6a97ef46b8f3678e15649473514b
suspendedtundra2@gmail.com <amogh@amxgh.com>
2026-01-21 20:22:53 -0600

    refactor: update other.py to use constants for ticket categories and colors

--- src/utils/ticket_utils/other.py
+++ src/utils/ticket_utils/other.py
@@ -1,13 +1,13 @@
 import discord
 
-from src.utils.consts import ticket_categories, neutral_color
+from src.utils.consts import TICKET_CATEGORIES, NEUTRAL_COLOR
 
 
 async def other(ticket: discord.TextChannel, interaction: discord.Interaction, user: discord.Member, ign: str,
                 uuid: str):
     await ticket.edit(name=f"other-{ign}", topic=f"{interaction.user.id if interaction else user.id}|",
                       category=discord.utils.get((interaction.guild if interaction else ticket.guild).categories,
-                                                 name=ticket_categories["generic"]))
+                                                 name=TICKET_CATEGORIES["generic"]))
     await ticket.send(embed=discord.Embed(title="This ticket has been created for an unknown reason!",
                                           description="Please specify why you have created this ticket!",
-                                          color=neutral_color))
+                                          color=NEUTRAL_COLOR))
