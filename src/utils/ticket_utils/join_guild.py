18560b1e9b3df1f416426eebbec70000ec84c26c
suspendedtundra2@gmail.com <amogh@amxgh.com>
2026-04-04 01:32:20 -0500

    refactor: update join_guild function to use TICKET_CATEGORY_IDS for category setting

--- src/utils/ticket_utils/join_guild.py
+++ src/utils/ticket_utils/join_guild.py
@@ -2,14 +2,13 @@ from __main__ import bot
 
 import discord
 
-from src.utils.consts import TICKET_CATEGORIES, JOIN_REQUEST_EMBED
+from src.utils.consts import JOIN_REQUEST_EMBED
 
 
 async def join_guild(ticket: discord.TextChannel, interaction: discord.Interaction, user: discord.Member, ign: str):
     # Edit category and send info embed with requirements
-    await ticket.edit(name=f"join-request-{ign}", topic=f"{interaction.user.id if interaction else user.id}|",
-                      category=discord.utils.get((interaction.guild if interaction else ticket.guild).categories,
-                                                 name=TICKET_CATEGORIES["registrees"]))
+    await ticket.edit(name=f"join-request-{ign}",
+                      category=bot.TICKET_CATEGORY_IDS["registrees"])
     await ticket.purge(limit=100)
     await ticket.send(
         embed=JOIN_REQUEST_EMBED.set_author(name=f"{ign} wishes to join Miscellaneous"))
