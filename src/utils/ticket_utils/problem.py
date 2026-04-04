33986f92a7ffda2400d7186f916eccba2625fc1b
suspendedtundra2@gmail.com <amogh@amxgh.com>
2026-04-04 01:33:11 -0500

    refactor: simplify problem function by removing category setting

--- src/utils/ticket_utils/problem.py
+++ src/utils/ticket_utils/problem.py
@@ -1,12 +1,10 @@
 import discord
 
-from src.utils.consts import NEUTRAL_COLOR, TICKET_CATEGORIES
+from src.utils.consts import NEUTRAL_COLOR
 
 
 async def problem(ticket: discord.TextChannel, interaction: discord.Interaction, user: discord.Member, ign: str):
-    await ticket.edit(name=f"problem-{ign}", topic=f"{interaction.user.id if interaction else user.id}|",
-                      category=discord.utils.get((interaction.guild if interaction else ticket.guild).categories,
-                                                 name=TICKET_CATEGORIES["generic"]))
+    await ticket.edit(name=f"problem-{ign}")
     await ticket.send(embed=discord.Embed(title=f"{ign} has a problem!",
                                           description="Please elaborate on your"
                                                       "problem so that the staff team can help you out!",
