ba0adf53daf2d6af14da369105184de3f449f234
suspendedtundra2@gmail.com <amogh@amxgh.com>
2026-04-04 01:31:49 -0500

    refactor: update clip_submission to set category using TICKET_CATEGORY_IDS

--- src/utils/ticket_utils/clip_submission.py
+++ src/utils/ticket_utils/clip_submission.py
@@ -2,15 +2,14 @@ from __main__ import bot
 
 import discord
 
-from src.utils.consts import TICKET_CATEGORIES, CLIP_SUBMISSION_TICKET
+from src.utils.consts import CLIP_SUBMISSION_TICKET
 
 
 async def clip_submission(ticket: discord.TextChannel, interaction: discord.Interaction, user: discord.Member,
                           ign: str):
     # Edit category and send info embed with requirements
-    await ticket.edit(name=f"YT-clip-{ign}", topic=f"{interaction.user.id if interaction else user.id}|",
-                      category=discord.utils.get((interaction.guild if interaction else ticket.guild).categories,
-                                                 name=TICKET_CATEGORIES["other"]))
+    await ticket.edit(name=f"YT-clip-{ign}",
+                      category=bot.TICKET_CATEGORY_IDS["milestone"])
     await ticket.set_permissions(bot.yt_channel_manager, send_messages=True, read_messages=True,
                                  add_reactions=True, embed_links=True,
                                  attach_files=True,
