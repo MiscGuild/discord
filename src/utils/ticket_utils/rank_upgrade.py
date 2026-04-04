92a9f749fc3a40b84c35b3cdb949b85fdfc1a701
suspendedtundra2@gmail.com <amogh@amxgh.com>
2026-04-04 01:33:23 -0500

    refactor: remove category setting from rank upgrade function

--- src/utils/ticket_utils/rank_upgrade.py
+++ src/utils/ticket_utils/rank_upgrade.py
@@ -1,13 +1,10 @@
 import discord
 
-from src.utils.consts import NEUTRAL_COLOR, TICKET_CATEGORIES
+from src.utils.consts import NEUTRAL_COLOR
 
 
 async def rank_upgrade(ticket: discord.TextChannel, interaction: discord.Interaction, user: discord.Member, ign: str):
-    await ticket.edit(name=f"rank-upgrade-{ign}", topic=f"{interaction.user.id if interaction else user.id}|",
-                      category=discord.utils.get((interaction.guild if interaction else ticket.guild).categories,
-                                                 name=TICKET_CATEGORIES[
-                                                     "generic"]))
+    await ticket.edit(name=f"rank-upgrade-{ign}")
     await ticket.send(embed=discord.Embed(title=f"{ign} claims to have won a rank upgrade.",
                                           description="Please provide a link to a message saying you won a rank upgrade.",
                                           color=NEUTRAL_COLOR).set_footer(
