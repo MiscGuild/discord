dcf4cefe67573df1063cee48b4c7a0d122db040e
suspendedtundra2@gmail.com <amogh@amxgh.com>
2026-04-12 17:17:59 -0500

    refactor: handle case when UUID resolution fails for Discord ID

--- src/utils/data_classes.py
+++ src/utils/data_classes.py
@@ -105,6 +105,9 @@ class RegisteredDiscordMember:
         from src.utils.request_utils import get_mojang_profile_from_uuid
 
         resolved_uuid = await get_uuid_from_discord_id(discord_id=discord_id)
+        if not resolved_uuid:
+            return cls(ign=None, uuid=resolved_uuid, discord_id=discord_id)
+
         ign, _ = await get_username_from_uuid(uuid=resolved_uuid)
         if not ign:
             ign, resolved_uuid = await get_mojang_profile_from_uuid(resolved_uuid)
