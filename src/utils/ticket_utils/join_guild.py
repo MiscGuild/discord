bd812282e78836bab7d93c56a3d5b148372b5e83
suspendedtundra2@gmail.com <amogh@amxgh.com>
2026-05-23 01:15:26 -0500

    refactor: enhance join request message UI with DesignerBuilder integration

--- src/utils/ticket_utils/join_guild.py
+++ src/utils/ticket_utils/join_guild.py
@@ -2,7 +2,8 @@ from __main__ import bot
 
 import discord
 
-from src.utils.consts import JOIN_REQUEST_EMBED
+from src.utils.consts import REQUIREMENTS_TEXT
+from src.utils.ui_utils import DesignerBuilder
 
 
 async def join_guild(ticket: discord.TextChannel, interaction: discord.Interaction, user: discord.Member, ign: str):
@@ -10,5 +11,17 @@ async def join_guild(ticket: discord.TextChannel, interaction: discord.Interacti
     await ticket.edit(name=f"join-request-{ign}",
                       category=bot.TICKET_CATEGORY_IDS["registrees"])
     await ticket.purge(limit=100)
-    await ticket.send(
-        embed=JOIN_REQUEST_EMBED.set_author(name=f"{ign} wishes to join Miscellaneous"))
+    builder = (
+        DesignerBuilder(timeout=None)
+        .container()
+        .text(f"# <@{user.id}> wishes to join Miscellaneous!")
+        .text(f"IGN: `{ign}`")
+        .text("## Requirements")
+        .text(REQUIREMENTS_TEXT)
+        .text(
+            "If you don't think you can meet the requirements, we'd recommend still applying. Worst case scenario, you get kicked for inactivity after a week or so, and you can always reapply later when you think you meet the requirements.")
+        .text(
+            "-# Please be patient while the staff team reviews your request. We will try to get back to you within a couple hours, but it may take longer during busy periods.")
+        .end()
+    )
+    await ticket.send(view=builder.build())
